2af90f95eae44fa2438692a7897f20bf287063c2
Nathaniel Deal <ndeal@rsglab.com>
2016-12-27 12:06:38 -0500

    Fix trigger switch reset switch

--- AmmoCounter_V1/AmmoCounter_V1_Trigger_Switch/AmmoCounter_V1_Trigger_Switch.ino
+++ AmmoCounter_V1/AmmoCounter_V1_Trigger_Switch/AmmoCounter_V1_Trigger_Switch.ino
@@ -1,5 +1,5 @@
 // AmmoCounter V1 with Trigger Switch
-// Updated 12/3/2016
+// Updated 12/27/2016
 // Created by: Nathaniel Deal
 // More Info: www.ammocounter.com 
 
@@ -16,29 +16,29 @@ AmmoCounter counter = AmmoCounter(); // Setup AmmoCounter instance
 
 // Setup Switches with Bounce2 Library
 #define toggleBtnpin 4
-//#define clipBtnpin 5
+#define resetBtnpin 5
 #define counterBtnpin 6
 Bounce toggleBtn = Bounce(); 
-//Bounce clipBtn = Bounce(); 
+Bounce resetBtn = Bounce(); 
 Bounce counterBtn = Bounce(); 
 
 void setup() {
 
   // Toggle/Reset/Counter Setup
   pinMode(toggleBtnpin, INPUT); 
-  // pinMode(clipBtnpin, INPUT); 
+  pinMode(resetBtnpin, INPUT); 
   pinMode(counterBtnpin, INPUT); 
 
   digitalWrite(toggleBtnpin, LOW);  // Activate internal pullup
-  // digitalWrite(clipBtnpin, LOW);  // Activate internal pullup
+  digitalWrite(resetBtnpin, LOW);  // Activate internal pullup
   digitalWrite(counterBtnpin, LOW);  // Activate internal pullup
 
   toggleBtn.attach(toggleBtnpin);
-  // clipBtn.attach(clipBtnpin);
+  resetBtn.attach(resetBtnpin);
   counterBtn.attach(counterBtnpin);
 
   toggleBtn.interval(5);
-  // clipBtn.interval(5);
+  resetBtn.interval(5);
   counterBtn.interval(5); 
 
   // Show Initial Count
@@ -100,19 +100,19 @@ void loop(){
       }
     }
     
-  // Monitor Clip Reset
+  // Monitor Reset Button
   //----------------------------------------------------//
     
-    // Check if clip is released
+    // Check if resetbutton is pressed
   
-//    if (clipBtn.update() ) 
-//    {
-//      if (clipBtn.read() == HIGH)  
-//      {
-//        count = toggleArray[togglePosition]; // Reset count
-//        counter.displayNumber(count); //Send to display  
-//      }
-//    }
+    if (resetBtn.update() ) 
+    {
+      if (resetBtn.read() == HIGH)  
+      {
+        count = toggleArray[togglePosition]; // Reset count
+        counter.displayNumber(count); //Send to display  
+      }
+    }
 }
 
 // Blink display and then auto-reset     
